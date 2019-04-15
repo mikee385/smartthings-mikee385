c5b3acc49106d7799f53d8dcbd687e9dccd9647d
Michael Pierce <mikee385@gmail.com>
2019-04-15 08:03:46 -0500

    Change Repeated Reminders to be a Parent-Child app

--- smartapps/mikee385/repeated-reminders.src/repeated-reminders.groovy
+++ smartapps/mikee385/repeated-reminders.src/repeated-reminders.groovy
@@ -13,14 +13,6 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-def getTurnedOn() {
-    return "Turned On"
-}
-
-def getTurnedOff() {
-    return "Turned Off"
-}
- 
 definition(
     name: "Repeated Reminders",
     namespace: "mikee385",
@@ -29,266 +21,21 @@ definition(
     category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Family/App-QualityTimeTracker.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Family/App-QualityTimeTracker@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Family/App-QualityTimeTracker@2x.png")
-
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Family/App-QualityTimeTracker@2x.png",
+    singleInstance: true)
 
 preferences {
-    page(name: "settings")
+    page(name:"settings")
 }
 
 def settings() {
-    dynamicPage(name: "settings", title: "", install: true, uninstall: true) {
-        section("Reminder Switch") {
-            input "reminderSwitch", "capability.switch", title: "Which switch should start and stop the reminder?", required: true, submitOnChange: true
-            if (reminderSwitch) input "reminderStart", "enum", title: "Start reminder when switch is:", options: [turnedOn, turnedOff], required: true, submitOnChange: true
-            if (reminderSwitch && reminderStart) {
-                if (reminderStart == turnedOn) {
-                    paragraph "Reminder will start when ${reminderSwitch} is turned on and will stop when ${reminderSwitch} is turned off."
-                } else {
-                    paragraph "Reminder will start when ${reminderSwitch} is turned off and will stop when ${reminderSwitch} is turned on."
-                }
-            }
-        }
-        if (reminderSwitch) {
-            section("Pause Switch") {
-                input "pauseSwitch", "capability.switch", title: "Which switch should pause and resume the reminder? (optional)", required: false, submitOnChange: true
-                if (pauseSwitch) input "reminderPause", "enum", title: "Pause reminder when switch is:", options: [turnedOn, turnedOff], required: true, submitOnChange: true
-                if (pauseSwitch && reminderPause) {
-                    if (reminderPause == turnedOn) {
-                        paragraph "Reminder will pause when ${pauseSwitch} is turned on and will resume when ${pauseSwitch} is turned off."
-                    } else {
-                        paragraph "Reminder will pause when ${pauseSwitch} is turned off and will resume when ${pauseSwitch} is turned on."
-                    }
-                }
-            }
-            section("Delay") {
-                input "initialDuration", "number", title: "How long to wait before repeated messages start (in seconds)?", required: true, defaultValue: 0
-            }
-            section("Repeated Messages") {
-                input "repeatedMessage", "text", title: "What message should be shown while the reminder is running?", required: true
-                input "includeDurationInMessage", "boolean", title: "Should the running time be appended to the message?", required: true, defaultValue: false, submitOnChange: true
-                if (includeDurationInMessage) input "durationResolution", "enum", title: "Minumum resolution for the running time:", options: ["Days", "Hours", "Minutes", "Seconds"], required: true, defaultValue: "Seconds"
-                input "repeatedDuration", "number", title: "How frequently should messages be shown (in seconds)?", required: true, defaultValue: 300
-            }
-            section("Optional Messages") {
-                input "startMessage", "text", title: "Message shown when the reminder starts:", required: false
-                input "stopMessage", "text", title: "Message shown when the reminder stops:", required: false
-                input "pauseMessage", "text", title: "Message shown when the reminder pauses:", required: false
-                input "resumeMessage", "text", title: "Message shown when the reminder resumes:", required: false
-            }        
-            section() {
-                label title: "Assign a name", required: true
-            }
+    dynamicPage(name: "settings", title: "Reminders", install: true, uninstall: true, submitOnChange: true) {
+        section() {
+            app(name: "Automations", appName: "Repeated Reminders - Child", namespace: "mikee385", title: "Add New Reminder", multiple: true)
         }
     }
 }
 
-def installed() {
-    log.debug "Installed with settings: ${settings}"
+def installed() {}
 
-    initialize()
-}
-
-def updated() {
-    log.debug "Updated with settings: ${settings}"
-
-    unsubscribe()
-    initialize()
-}
-
-def initialize() {
-    subscribe(reminderSwitch, "switch.on", reminderSwitchOnHandler)
-    subscribe(reminderSwitch, "switch.off", reminderSwitchOffHandler)
-    subscribe(pauseSwitch, "switch.on", pauseSwitchOnHandler)
-    subscribe(pauseSwitch, "switch.off", pauseSwitchOffHandler)
-    
-    state.startTime = now()
-}
-
-def reminderSwitchOnHandler(evt) {
-    log.debug "${evt.displayName}'s ${evt.name} is ${evt.value}"
-    
-    if (reminderStart == turnedOn) {
-        start()
-    } else {
-        stop()
-    }
-}
-
-def reminderSwitchOffHandler(evt) {
-    log.debug "${evt.displayName}'s ${evt.name} is ${evt.value}"
-    
-    if (reminderStart == turnedOn) {
-        stop()
-    } else {
-        start()
-    }
-}
-
-def pauseSwitchOnHandler(evt) {
-    log.debug "${evt.displayName}'s ${evt.name} is ${evt.value}"
-    
-    if (reminderPause == turnedOn) {
-        pause()
-    } else {
-        resume()
-    }
-}
-
-def pauseSwitchOffHandler(evt) {
-    log.debug "${evt.displayName}'s ${evt.name} is ${evt.value}"
-    
-    if (reminderPause == turnedOn) {
-        resume()
-    } else {
-        pause()
-    }
-}
-
-def start() {
-    if (!isPaused) {
-        log.debug "Reminder has started"
-        
-        if (startMessage) {
-            sendPush(startMessage)
-        }
-        begin()
-    } else {
-        log.debug "Reminder was previously paused"
-    }
-}
-
-def stop() {
-    log.debug "Reminder has stopped"
-    
-    if (stopMessage) {
-        sendPush(stopMessage)
-    }
-    end()
-}
-
-def pause() {
-    if (isStarted) {
-        log.debug "Reminder has been paused"
-        
-        if (pauseMessage) {
-            sendPush(pauseMessage)
-        }
-        end()
-    } else {
-        log.debug "Reminder was not previously started"
-    }  
-}
-
-def resume() {
-    if (isStarted) {
-        log.debug "Reminder has been resumed"
-        
-        if (resumeMessage) {
-            sendPush(resumeMessage)
-        }
-        begin()
-    } else {
-        log.debug "Reminder was not previously started"
-    }    
-}
-
-def begin() {
-    state.startTime = now()
-    
-    if (initialDuration > 0) {
-        runIn(initialDuration, repeat)
-    } else {
-        repeat()
-    }
-}
-
-def end() {
-    unschedule()
-}
-
-def repeat() {
-    log.debug "Reminder has been running for ${now() - state.startTime} milliseconds"
-
-    def message = repeatedMessage
-    if (includeDurationInMessage) {    
-        def totalMilliseconds = now() - state.startTime        
-        def milliseconds = (int)(totalMilliseconds % 1000)
-        
-        def totalSeconds = (int)Math.floor((totalMilliseconds - milliseconds) / 1000)
-        def seconds = totalSeconds % 60
-        def totalMinutes = (int)Math.floor((totalSeconds - seconds) / 60)
-        def minutes = totalMinutes % 60
-        def totalHours = (int)Math.floor((totalMinutes - minutes) / 60)
-        def hours = totalHours % 24
-        def days = (int)Math.floor((totalHours - hours) / 24)
-        
-        def durationString = ""
-        if (days || durationResolution == "Days") {
-            durationString += "${days} day"
-            if (days != 1) {
-                durationString += "s"
-            }
-        }
-        if (durationResolution != "Days") {
-            if (hours || durationResolution == "Hours" ) {
-                if (durationString) {
-                    durationString += ", "
-                }
-                durationString += "${hours} hour"
-                if (hours != 1) {
-                    durationString += "s"
-                }
-            }
-            if (durationResolution != "Hours") {
-                if (minutes || durationResolution == "Minutes") {
-                    if (durationString) {
-                        durationString += ", "
-                    }
-                    durationString += "${minutes} minute"
-                    if (minutes != 1) {
-                        durationString += "s"
-                    }
-                }
-                if (durationResolution != "Minutes") {
-                    if (seconds || durationResolution == "Seconds") {
-                        if (durationString) {
-                            durationString += ", "
-                        }
-                        durationString += "${seconds} second"
-                        if (seconds != 1) {
-                            durationString += "s"
-                        }
-                    }
-                }
-            }
-        }
-        
-        if (durationString) {
-            message = message + " (" + durationString + ")"
-        }
-    }
-    
-    sendPush(message)
-    runIn(repeatedDuration, repeat)
-}
-
-def getIsStarted() {
-    if (reminderStart == turnedOn) {
-        return reminderSwitch.currentSwitch == "on"
-    } else {
-        return reminderSwitch.currentSwitch == "off"
-    }
-}
-
-def getIsPaused() {
-    if (pauseSwitch) {
-        if (reminderPause == turnedOn) {
-            return pauseSwitch.currentSwitch == "on"
-        } else {
-            return pauseSwitch.currentSwitch == "off"
-        }
-    } else {
-        return false
-    }
-}
+def updated() {}
